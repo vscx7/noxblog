7d066c8c71b6fc6c1cd3ed4ce2877c3377a2edce
vscx7 <chaudharyvisha0457@gmail.com>
2024-08-23 22:52:37 +0530

    moving to a new remote repostory

---
+++ android/app/src/main/kotlin/com/example/noxblog/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.noxblog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:noxblog/core/common/loader.dart';
+import 'package:noxblog/core/common/sign_in_button.dart';
+import 'package:noxblog/core/constants/constants.dart';
+import 'package:noxblog/features/auth/controlller/auth_controller.dart';
+import 'package:noxblog/responsive/responsive.dart';
+
+class LoginScreen extends ConsumerWidget {
+  const LoginScreen({super.key});
+
+  void signInAsGuest(WidgetRef ref, BuildContext context) {
+    ref.read(authControllerProvider.notifier).signInAsGuest(context);
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => signInAsGuest(ref, context),
+            child: const Text(
+              'Sneek Peek',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: isLoading
+          ? const Loader()
+          : Column(
+              children: [
+                const SizedBox(height: 30),
+                const Text(
+                  'Read Write Repeat',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    Constants.loginEmotePath,
+                    height: 450,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Responsive(child: SignInButton()),
+              ],
+            ),
+    );
+  }
+}
